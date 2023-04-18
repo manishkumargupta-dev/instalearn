d294498a5c3987e31d7ffe80be1bff18c87d667f
mkg101 <manishkumargupta875@gmail.com>
2023-04-18 17:45:33 +0530

    profile and not found pages completed

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useEffect } from "react";
+import Header from "../components/header";
 
-function Notfound() {
-  return <div>Notfound</div>;
+export default function NotFound() {
+  useEffect(() => {
+    document.title = "Not Found - Instagram";
+  }, []);
+
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
+    </div>
+  );
 }
-
-export default Notfound;
