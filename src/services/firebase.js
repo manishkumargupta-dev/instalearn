9a2863d8f9b76298ad5e5dad017df4726ffc4f01
mkg101 <manishkumargupta875@gmail.com>
2023-04-20 14:04:02 +0530

    dashboard sidebar completed

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -69,7 +69,54 @@ export async function isUserFollowingProfile(
 
   return response.userId;
 }
+export async function getSuggestedProfiles(userId, following) {
+  let query = firebase.firestore().collection("users");
 
+  if (following.length > 0) {
+    query = query.where("userId", "not-in", [...following, userId]);
+  } else {
+    query = query.where("userId", "!=", userId);
+  }
+  const result = await query.limit(10).get();
+
+  const profiles = result.docs.map((user) => ({
+    ...user.data(),
+    docId: user.id,
+  }));
+
+  return profiles;
+}
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId, // currently logged in user document id (karl's profile)
+  profileId, // the user that karl requests to follow
+  isFollowingProfile // true/false (am i currently following this person?)
+) {
+  return firebase
+    .firestore()
+    .collection("users")
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowingProfile
+        ? FieldValue.arrayRemove(profileId)
+        : FieldValue.arrayUnion(profileId),
+    });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId, // currently logged in user document id (karl's profile)
+  loggedInUserDocId, // the user that karl requests to follow
+  isFollowingProfile // true/false (am i currently following this person?)
+) {
+  return firebase
+    .firestore()
+    .collection("users")
+    .doc(profileDocId)
+    .update({
+      followers: isFollowingProfile
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId),
+    });
+}
 export async function toggleFollow(
   isFollowingProfile,
   activeUserDocId,
